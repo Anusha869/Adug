a587a71e7caa55e25b79341f553a53333421c494
Anusha869 <84143810+Anusha869@users.noreply.github.com>
2021-05-15 14:01:03 +0530

    Add files via upload

---
+++ Details.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Details.aspx.cs" Inherits="adug.Details" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="auto-style1">
+        <asp:DropDownList ID="DropDownList1" runat="server">
+            <asp:ListItem>Select the Item
+
+            </asp:ListItem>
+            <asp:ListItem>Vigo</asp:ListItem>
+        </asp:DropDownList>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace adug
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Login_click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-OIDLIMN\\SQLEXPRESS1;Initial Catalog=adug3;Integrated Security=True");
+             con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from login where Username='" + TextBox1.Text + "' and Password ='" + TextBox2.Text + "'", con);
+             SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+          if (dt.Rows.Count > 0)
+           {
+            Response.Redirect("Details.aspx");
+           }
+          else
+         {
+            Response.Write("<script>alert('Please enter valid Username and Password')</script>");
+         }
+        }
+
+            
+    }
+}
